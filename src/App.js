481bd8a8a6ae2d99ddeb734dabf132cb8c05bb7b
Nizamph <116118003+Nizamph@users.noreply.github.com>
2022-12-25 13:42:21 +0530

    made close button of the Modal works and we can open it by clicking cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import { Fragment } from "react";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 function App() {
+     const [cartIsShown, setCartIsShown] = useState(false)
+
+     const showCartHandler = () => {
+         setCartIsShown(true)
+     }
+
+     const hideShowCartHandler = () => {
+      setCartIsShown(false)
+     }
   return (
     <Fragment>
-      <Cart/>
-      <Header/>
+      {cartIsShown && <Cart onHideCart={hideShowCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
      <Meals/>
       </main>
